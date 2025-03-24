6f5e8a3956ccf44d47a9b40db5ad421c78093b10
angel <angeliani.junitaviana@gmail.com>
2025-03-24 15:26:03 +0700

    Add files via upload

---
+++ src/praktic/inheritance/transportation/Taxi.java
@@ -0,0 +1,18 @@
+package praktic.inheritance.transportation;
+
+public class Taxi extends Vehicle {
+    // konstanta statis untuk tarif per kilometer
+    public static final int FARE_PER_KM = 5000; // tarif per km adalah Rp5000
+    public final int routeDistance; // jarak tempuh taksi dalam kilometer
+
+    // konstruktor taksi yang memanggil konstruktor superclass Vehicle
+    public Taxi(String name, int capacity, String route, int routeDistance) {
+        super(name, capacity, route); // memanggil konstruktor Vehicle
+        this.routeDistance = routeDistance;
+    }
+
+    // method untuk menghitung tarif taksi berdasarkan jarak tempuh
+    public int calculateFare() {
+        return routeDistance * FARE_PER_KM; // tarif per km = Rp5000
+    }
+}
